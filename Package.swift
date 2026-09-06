b22ad7f171962c92c3f199443711e5a2c7eb1786
Elfaria <319984589+ElfariaKensinton@users.noreply.github.com>
2026-09-06 19:59:01 +0300

    Add service execution helper target

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         .executable(name: "systemd", targets: ["SystemdDaemon"]),
         .executable(name: "systemctl", targets: ["Systemctl"]),
         .executable(name: "journalctl", targets: ["Journalctl"]),
+        .executable(name: "systemd-exec-helper", targets: ["SystemdExecHelper"]),
         .library(name: "SystemdCore", targets: ["SystemdCore"])
     ],
     targets: [
@@ -17,6 +18,7 @@ let package = Package(
         .executableTarget(name: "SystemdDaemon", dependencies: ["SystemdCore"]),
         .executableTarget(name: "Systemctl", dependencies: ["SystemdCore"]),
         .executableTarget(name: "Journalctl", dependencies: ["SystemdCore"]),
+        .executableTarget(name: "SystemdExecHelper"),
         .testTarget(name: "SystemdCoreTests", dependencies: ["SystemdCore"])
     ]
 )
