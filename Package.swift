dc1ec7865c6a2a32e91b61e59c2b9f921dc570af
Elfaria <319984589+ElfariaKensinton@users.noreply.github.com>
2026-09-06 17:17:08 +0300

    Add journalctl executable

--- Package.swift
+++ Package.swift
@@ -9,12 +9,14 @@ let package = Package(
     products: [
         .executable(name: "systemd", targets: ["SystemdDaemon"]),
         .executable(name: "systemctl", targets: ["Systemctl"]),
+        .executable(name: "journalctl", targets: ["Journalctl"]),
         .library(name: "SystemdCore", targets: ["SystemdCore"])
     ],
     targets: [
         .target(name: "SystemdCore"),
         .executableTarget(name: "SystemdDaemon", dependencies: ["SystemdCore"]),
         .executableTarget(name: "Systemctl", dependencies: ["SystemdCore"]),
+        .executableTarget(name: "Journalctl", dependencies: ["SystemdCore"]),
         .testTarget(name: "SystemdCoreTests", dependencies: ["SystemdCore"])
     ]
 )
